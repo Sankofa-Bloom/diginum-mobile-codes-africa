66054cc7e8ad21b821241701717a4685ee17b138
Sankofa Bloom <sankofabloom@Sankofas-iMac.local>
2025-08-20 18:08:22 -0700

    Debug mtn-momo Collections constructor issue - Add logging to see actual module structure and try different import patterns

--- netlify/functions/utils/momoClient.js
+++ netlify/functions/utils/momoClient.js
@@ -1,13 +1,23 @@
-const { Collections } = require('mtn-momo');
+const MtnMomo = require('mtn-momo');
 
 let momoClient = null;
 
 /**
  * Get or create MTN MoMo client instance
- * @returns {Collections} MoMo client instance
+ * @returns {Object} MoMo client instance
  */
 function getMomoClient() {
   if (!momoClient) {
+    console.log('MTN MoMo module structure:', Object.keys(MtnMomo));
+    
+    // Try different ways to access Collections
+    const Collections = MtnMomo.Collections || MtnMomo.default?.Collections || MtnMomo;
+    
+    if (typeof Collections !== 'function') {
+      console.error('Collections is not a constructor. Available methods:', Object.keys(Collections || {}));
+      throw new Error('Collections is not a constructor. Available: ' + Object.keys(Collections || {}).join(', '));
+    }
+    
     // Initialize client with environment variables
     momoClient = new Collections({
       callbackUrl: process.env.MTN_MOMO_CALLBACK_URL,
